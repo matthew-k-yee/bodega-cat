e14abe476317dc35c9ad9fe9a9ee52aacd2a1431
Leonardo Rodriguez <leo@MacBook-Pro.local>
2019-04-27 19:35:09 -0400

    reverted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import BusinessForm from './components/BusinessForm'
 
 function App() {
   return (
     <div className="App">
       Bodega Cats
-      <BusinessForm />
     </div>
   );
 }
