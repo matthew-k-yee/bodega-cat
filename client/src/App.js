0380e06c82347fb37823492474f98e207e0f9c1b
Leonardo Rodriguez <leo@MacBook-Pro.local>
2019-04-28 04:15:07 -0400

    reverted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import AddressForm from './components/AddressForm';
-import UserSignIn from './components/UserSignIn'
+import AddressForm from './components/AddressForm';
+
 
 function App() {
   return (
     <div className="App">
       Bodega Cats
-      {/* <AddressForm/> */}
-      <UserSignIn/>
+      <AddressForm/>
     </div>
   );
 }
