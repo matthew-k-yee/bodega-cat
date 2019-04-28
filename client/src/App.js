a88605cd33bf2bd86db391746c1bcc99661ea58a
Matthew Yee <matthewyee@Jacky-Man.local>
2019-04-28 12:15:31 -0400

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
 import LandingPage from './components/LandingPage';
 import SignUpContainer from './components/SignUpContainer';
 
